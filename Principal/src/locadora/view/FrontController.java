b1c2d397b2a8c22668e7180546c708e1a937bfb4
Valdir Junior <valdir.junior90@hotmail.com>
2020-07-18 16:15:09 -0300

    Tela de login abrindo

---
+++ Principal/src/locadora/view/FrontController.java
@@ -0,0 +1,16 @@
+package locadora.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class FrontController {
+	@FXML private TextField login;
+	@FXML private PasswordField senha;
+	@FXML private Label erroAut;
+	public void autenticar(ActionEvent e) {
+		
+	}
+}
